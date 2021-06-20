519feea9ffcd248c50628bae55d48b71913eb9d1
Martin Šimon <72451051+Granc3k@users.noreply.github.com>
2021-06-20 18:30:26 +0000

    Initial commit

---
+++ Fizzbuzz + string art/string_art.py
@@ -0,0 +1,29 @@
+
+from graphics import Canvas
+
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
+
+LINE_SPACING = 20
+
+NUM_LINES = CANVAS_WIDTH // LINE_SPACING
+
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    canvas.set_canvas_title("String Art")
+    x1 = 0
+    y1 = 0
+    x2 = 0 + LINE_SPACING
+    y2 = CANVAS_HEIGHT
+    for i in range(NUM_LINES):
+        canvas.create_line(x1, y1, x2, y2)
+        y1 += LINE_SPACING
+        x2 += LINE_SPACING
+
+
+    canvas.mainloop()
+
+
+if __name__ == "__main__":
+    main()
